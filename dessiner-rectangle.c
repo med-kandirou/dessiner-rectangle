302a36f7ed79fea60a9c2c49299b409c51f6aed3
kandirou0 <simo.kandirou15@gmail.com>
2022-09-07 17:21:03 +0100

    update

---
+++ dessiner-rectangle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+main()
+{
+
+    int ligne;
+    int colone;
+
+    printf("saisir le nombre de ligne : ");
+    scanf("%d",&ligne);
+
+    printf("saisir le nombre de colone : ");
+    scanf("%d",&colone);
+
+    for(int i=1;i<=ligne;i++){
+        printf("\n");
+        for(int j=1;j<=colone;j++){
+            if(j==1 || j==colone || i==1 || i==ligne ){
+                printf("* \t");
+            }
+            else{
+                printf(" \t");
+            }
+        }
+    }
+    return 0;
+
+
+
+    }
+
